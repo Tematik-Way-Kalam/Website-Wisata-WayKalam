0b1e82f2f5b5b690b5f73fb144fe11077934991c
TobyM27 <tobyanto.121140099@student.itera.ac.id>
2024-01-12 10:29:29 +0700

    tambah file 404

---
+++ client/404.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Ahh Snap</title>
+
+    <!-- CSS Style -->
+    <style>
+        /* Error Setup */
+        .err{
+         height: 100%;
+
+         display : flex; 
+         flex-direction: column;
+         align-items: center;
+        
+         /* Font settings */
+         font-family: 'Courier New', Courier, monospace;
+         font-style: normal;
+        }
+        /* Images Setup */
+        img{
+            height: 30em;
+            width : 30em;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Navbar -->
+
+    <!-- Error Images etc -->
+    <div class="err">
+        <img src="http://dev.b2c.tourvisio.com/cms/DefaultSanTSG/b2cImages/B2C_San_Default_images/Error%20Images/404.png" alt="Images Error">
+        <h1>404 Not Found</h1>
+        <h2>Silahkan Hubungkan ke Internet :)</h2>
+    </div>
+</body>
+</html>
