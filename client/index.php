9adfc24c34249573a93529f81989f307a52ab36c
yondikavl <yondikaviolanda@gmail.com>
2024-01-13 10:59:49 +0700

    update visitor landing

--- client/index.php
+++ client/index.php
@@ -103,23 +103,36 @@
                 <h2>Tentang</h2>
             </td>
             <td class="td2">
-                <p>Desa Wisata Way Kalam adalah salah satu dari sekian banyak desa wisata yang berkembang di wilayah Kabupaten Lampung Selatan. Berlokasi di kawasan lereng gunung Rajabasa (salah satu gunung yang terkenal di Kabupaten Lampung Selatan)  Desa Wisata Way Kalam adalah salah satu dari sekian banyak desa wisata yang berkembang di wilayah Kabupaten Lampung Selatan. Berlokasi di kawasan lereng gunung Rajabasa (salah satu gunung yang terkenal di Kabupaten Lampung Selatan) </p>
+                <p>Desa Wisata Way Kalam adalah salah satu dari sekian banyak desa wisata yang berkembang di wilayah Kabupaten Lampung Selatan. Berlokasi di kawasan lereng gunung Rajabasa (salah satu gunung yang terkenal di Kabupaten Lampung Selatan) Desa Wisata Way Kalam adalah salah satu dari sekian banyak desa wisata yang berkembang di wilayah Kabupaten Lampung Selatan. Berlokasi di kawasan lereng gunung Rajabasa (salah satu gunung yang terkenal di Kabupaten Lampung Selatan) </p>
                 <a type="button" class="nav-link btn btn-primary p-2" href="#" style="background: rgb(19,39,88); background: linear-gradient(313deg, rgba(19,39,88,1) 0%, rgba(0,166,147,1) 100%); color:white;">
-                Lihat Selengkapnya <svg xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 25" fill="none"> <path d="M12 22.4594C17.5228 22.4594 22 17.9822 22 12.4594C22 6.9365 17.5228 2.45935 12 2.45935C6.47715 2.45935 2 6.9365 2 12.4594C2 17.9822 6.47715 22.4594 12 22.4594Z" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> <path d="M12 16.4594L16 12.4594L12 8.45935" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> <path d="M8 12.4594H16" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                    Lihat Selengkapnya <svg xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 25" fill="none">
+                        <path d="M12 22.4594C17.5228 22.4594 22 17.9822 22 12.4594C22 6.9365 17.5228 2.45935 12 2.45935C6.47715 2.45935 2 6.9365 2 12.4594C2 17.9822 6.47715 22.4594 12 22.4594Z" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M12 16.4594L16 12.4594L12 8.45935" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M8 12.4594H16" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
             </td>
         </table>
-        <table class="table2">
-            <td class="td3">
-                <video src="bg/JJ.mp4" width="810" height="457" controls></video>
-            </td>
-            <td class="td4">
-                <img src="bg/visit.jpg" width="457" height="457" alt="visitor">
-            </td>
+        <table class="table2 d-flex justify-content-center align-items-center py-5">
+            <tr class="d-flex align-items-center gap-5 mt-5">
+                <td class="d-flex align-items-center">
+                    <video src="bg/JJ.mp4" width="810" height="457" controls class="mr-5 rounded-5"></video>
+                </td>
+                <td id="visitor" class="text-white d-flex flex-column justify-content-center align-items-center rounded-5" style="background-color: #132758; background-image: url(./bg/visit.png); background-repeat: no-repeat; background-size: cover; min-width: 457px; min-height: 457px;">
+                    <div class="d-flex justify-content-center flex-column align-items-center">
+                        <p style="font-size: 64px; font-family: 'Big Shoulders Display', sans-serif; font-weight: 900; background: var(--p2, linear-gradient(137deg, #fff 24.22%, #7cdbf0 105.58%)); background-clip: text; -webkit-background-clip: text; -webkit-text-fill-color: transparent;">2000+</p>
+                        <p style="font-size: 20px;">Pengunjung Air Terjun</p>
+                    </div>
+                    <div class="d-flex justify-content-center flex-column align-items-center">
+                        <p style="font-size: 64px; font-family: 'Big Shoulders Display', sans-serif; font-weight: 900; background: var(--p2, linear-gradient(137deg, #fff 24.22%, #7cdbf0 105.58%)); background-clip: text; -webkit-background-clip: text; -webkit-text-fill-color: transparent;">22+</p>
+                        <p style="font-size: 20px;">Homestay</p>
+                    </div>
+                </td>
+            </tr>
         </table>
     </section>
 
     <!-- section 2 -->
-    <section id = "section2">
+    <section id="section2">
         <div class="wisata">
             <div class="ex1">
                 <p class="teks-wisata">Wisata</p>
@@ -128,36 +141,40 @@
             <div class="ex2">
                 <p class="teks-desc">Desa Wisata Way Kalam adalah salah satu dari sekian banyak desa wisata yang berkembang di wilayah Kabupaten Lampung Selatan. Berlokasi di kawasan lereng gunung Rajabasa</p>
                 <a type="button" class="nav-link btn btn-primary p-2" href="#" style="background: var(--pu2, #00A693); color:white;">
-                Lihat Selengkapnya<svg xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 25" fill="none"> <path d="M12 22.4594C17.5228 22.4594 22 17.9822 22 12.4594C22 6.9365 17.5228 2.45935 12 2.45935C6.47715 2.45935 2 6.9365 2 12.4594C2 17.9822 6.47715 22.4594 12 22.4594Z" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> <path d="M12 16.4594L16 12.4594L12 8.45935" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> <path d="M8 12.4594H16" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg> </a> 
+                    Lihat Selengkapnya<svg xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 25" fill="none">
+                        <path d="M12 22.4594C17.5228 22.4594 22 17.9822 22 12.4594C22 6.9365 17.5228 2.45935 12 2.45935C6.47715 2.45935 2 6.9365 2 12.4594C2 17.9822 6.47715 22.4594 12 22.4594Z" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M12 16.4594L16 12.4594L12 8.45935" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M8 12.4594H16" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg> </a>
             </div>
         </div>
 
         <div class="cardboard">
             <!-- Card 1 -->
-        <div class="card" id = "wisata" style="width: 18rem;">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Red_Kitten_01.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Air Terjun Way Kalam (Induk)</h5>
-                <p class="card-text">Infokan animasi yang bagus</p>
-            <a href="#" class="btn btn-primary">Disini ya!</a>
+            <div class="card" id="wisata" style="width: 18rem;">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Red_Kitten_01.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Air Terjun Way Kalam (Induk)</h5>
+                    <p class="card-text">Infokan animasi yang bagus</p>
+                    <a href="#" class="btn btn-primary">Disini ya!</a>
+                </div>
             </div>
-        </div>
-        <!-- Card 2 -->
-        <div class="card" id = "wisata" style="width: 18rem;">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Red_Kitten_01.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Air Terjun Way Kalam (Induk)</h5>
-                <p class="card-text">Infokan animasi yang bagus</p>
-            <a href="#" class="btn btn-primary">Disini ya!</a>
+            <!-- Card 2 -->
+            <div class="card" id="wisata" style="width: 18rem;">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Red_Kitten_01.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Air Terjun Way Kalam (Induk)</h5>
+                    <p class="card-text">Infokan animasi yang bagus</p>
+                    <a href="#" class="btn btn-primary">Disini ya!</a>
+                </div>
             </div>
-        </div>
-        <!-- Card 3 -->
-        <div class="card" id = "wisata" style="width: 18rem;">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Red_Kitten_01.jpg" alt="...">
-            <h5 class="cardTitle">Air Terjun Way Kalam (Induk)</h5>
-            <p class="cardText">Infokan animasi yang bagus</p>
-            <a href="#" class="btn btn-primary">Disini ya!</a>
-        </div>
+            <!-- Card 3 -->
+            <div class="card" id="wisata" style="width: 18rem;">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Red_Kitten_01.jpg" alt="...">
+                <h5 class="cardTitle">Air Terjun Way Kalam (Induk)</h5>
+                <p class="cardText">Infokan animasi yang bagus</p>
+                <a href="#" class="btn btn-primary">Disini ya!</a>
+            </div>
 
         </div>
 
@@ -165,11 +182,15 @@
             <div class="direction">
                 <p class="teks-produk">PRODUK UKM DESA</p>
                 <p class="teks-dukungUMKM">DUKUNG UMKM DESA WISATA WAY KALAM</p>
-                <div style="">
-                <a type="button" class="nav-link btn btn-primary p-2" href="#" style="background: var(--pu2, #00A693); color:white;">
-                Lihat Selengkapnya<svg xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 25" fill="none"> <path d="M12 22.4594C17.5228 22.4594 22 17.9822 22 12.4594C22 6.9365 17.5228 2.45935 12 2.45935C6.47715 2.45935 2 6.9365 2 12.4594C2 17.9822 6.47715 22.4594 12 22.4594Z" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> <path d="M12 16.4594L16 12.4594L12 8.45935" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> <path d="M8 12.4594H16" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg> </a> 
+                <div>
+                    <a type="button" class="nav-link btn btn-primary p-2" href="#" style="background: var(--pu2, #00A693); color:white;">
+                        Lihat Selengkapnya<svg xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 25" fill="none">
+                            <path d="M12 22.4594C17.5228 22.4594 22 17.9822 22 12.4594C22 6.9365 17.5228 2.45935 12 2.45935C6.47715 2.45935 2 6.9365 2 12.4594C2 17.9822 6.47715 22.4594 12 22.4594Z" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M12 16.4594L16 12.4594L12 8.45935" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M8 12.4594H16" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg> </a>
                 </div>
-                
+
             </div>
             <div class="cardukm">
                 <div class="card">
@@ -185,49 +206,53 @@
                     <h4>PRODUKSI MADU KELULUT</h4>
                 </div>
             </div>
-        </div> 
+        </div>
     </section>
 
     <section id="section3">
-            <div class="layer1">
-                <div class="banner">
-                    <img src="../client/bg/piagam1.jpeg" alt="contoh gambar"> 
-                </div>
-                <div class="explain">
-                    <p class="main-heading">PENGHARGAAN</p>
-                    <p class="sub-heading">MASUK KEDALAM 300 DESA WISATA TERBAIK DI INDONESIA</p>
-                    <p class="explain-text">Desa Wisata Way Kalam adalah salah satu dari sekian banyak desa wisata yang berkembang di wilayah Kabupaten Lampung Selatan.  Berlokasi di kawasan lereng gunung Rajabasa.</p>
-                </div>
+        <div class="layer1">
+            <div class="banner">
+                <img src="../client/bg/piagam1.jpeg" alt="contoh gambar">
             </div>
+            <div class="explain">
+                <p class="main-heading">PENGHARGAAN</p>
+                <p class="sub-heading">MASUK KEDALAM 300 DESA WISATA TERBAIK DI INDONESIA</p>
+                <p class="explain-text">Desa Wisata Way Kalam adalah salah satu dari sekian banyak desa wisata yang berkembang di wilayah Kabupaten Lampung Selatan.  Berlokasi di kawasan lereng gunung Rajabasa.</p>
+            </div>
+        </div>
 
-            <div class="layer2">
-                <div class="explain">
-                    <p class="main-heading">PENGHARGAAN</p>
-                    <p class="sub-heading">MASUK KEDALAM 300 DESA WISATA TERBAIK DI INDONESIA</p>
-                    <p class="explain-text">Desa Wisata Way Kalam adalah salah satu dari sekian banyak desa wisata yang berkembang di wilayah Kabupaten Lampung Selatan.  Berlokasi di kawasan lereng gunung Rajabasa.</p>
-                </div>
-                <div class="banner">
-                    <img src="../client/bg/piagam2.jpeg" alt="piagam 2" />
-                </div>
+        <div class="layer2">
+            <div class="explain">
+                <p class="main-heading">PENGHARGAAN</p>
+                <p class="sub-heading">MASUK KEDALAM 300 DESA WISATA TERBAIK DI INDONESIA</p>
+                <p class="explain-text">Desa Wisata Way Kalam adalah salah satu dari sekian banyak desa wisata yang berkembang di wilayah Kabupaten Lampung Selatan.  Berlokasi di kawasan lereng gunung Rajabasa.</p>
             </div>
+            <div class="banner">
+                <img src="../client/bg/piagam2.jpeg" alt="piagam 2" />
+            </div>
+        </div>
 
-        </section>
-        <section class = "added">
-            <div class="moreopt">
-                <div class="exp3">
-                    <p class="teks-penghargaan">LIHAT PENGHARGAAN DESA WAY KALAM LEBIH BANYAK</p>
-                </div>
-                <div class="exp4">
-                    <p class="teks-moreopt">Desa Wisata Way Kalam adalah salah satu dari sekian banyak desa wisata yang berkembang di wilayah Kabupaten</p>
-                    <a type="button" class="nav-link btn btn-primary p-2" href="#" style="background: var(--pu2, #00A693); color:white; display: flex; padding: 15px 20px;justify-content: center;align-items: center;gap: 10px;">Lihat Selengkapnya<svg xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 25" fill="none"> <path d="M12 22.4594C17.5228 22.4594 22 17.9822 22 12.4594C22 6.9365 17.5228 2.45935 12 2.45935C6.47715 2.45935 2 6.9365 2 12.4594C2 17.9822 6.47715 22.4594 12 22.4594Z" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> <path d="M12 16.4594L16 12.4594L12 8.45935" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> <path d="M8 12.4594H16" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg> </a> 
-                    <!-- <a href="" class = "btn btn-primary">Hore udah kelar</a> -->
-                </div>
+    </section>
+    <section class="added">
+        <div class="moreopt">
+            <div class="exp3">
+                <p class="teks-penghargaan">LIHAT PENGHARGAAN DESA WAY KALAM LEBIH BANYAK</p>
             </div>
-        </section>
+            <div class="exp4">
+                <p class="teks-moreopt">Desa Wisata Way Kalam adalah salah satu dari sekian banyak desa wisata yang berkembang di wilayah Kabupaten</p>
+                <a type="button" class="nav-link btn btn-primary p-2" href="#" style="background: var(--pu2, #00A693); color:white; display: flex; padding: 15px 20px;justify-content: center;align-items: center;gap: 10px;">Lihat Selengkapnya<svg xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 25" fill="none">
+                        <path d="M12 22.4594C17.5228 22.4594 22 17.9822 22 12.4594C22 6.9365 17.5228 2.45935 12 2.45935C6.47715 2.45935 2 6.9365 2 12.4594C2 17.9822 6.47715 22.4594 12 22.4594Z" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M12 16.4594L16 12.4594L12 8.45935" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M8 12.4594H16" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg> </a>
+                <!-- <a href="" class = "btn btn-primary">Hore udah kelar</a> -->
+            </div>
+        </div>
+    </section>
 
     <!-- Section 4 (Testimonial) -->
     <section id="section4">
-        <div class="judultest" align= "center">
+        <div class="judultest" align="center">
             <p class="text-testimonial">TESTIMONIAL</p>
             <p class="text-katamereka">APA KATA MEREKA?</p>
         </div>
@@ -237,7 +262,7 @@
     </section>
 
     <!--  Section 5 (Penghargaan) -->
-    <section id="section5" class = "archive">
+    <section id="section5" class="archive">
         <div id="inner-section5">
             <div align="center" class="teks-map">
                 <p class=fs-2> MAP INTERAKTIF</p>
@@ -258,7 +283,7 @@
                     <p class="kunjungi-dan-rasakan">KUNJUNGI DAN RASAKAN <br>KEINDAHAN ALAM</p>
                 </div>
             </div>
-            
+
             <div class="contact-info">
                 <p>Way Kalam, Kec. Penengahan, Kabupaten Lampung Selatan, Lampung 35592</p>
                 <p>083773473484</p>
@@ -271,7 +296,7 @@
             </div>
         </div>
 
-        <hr class = "border-tebal">
+        <hr class="border-tebal">
 
         <div>
             <div class="down">
