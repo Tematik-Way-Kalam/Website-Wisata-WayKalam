c236738d2d94470b318a73f7ee9341e3a7f5c4e4
yondikavl <yondikaviolanda@gmail.com>
2024-01-14 15:39:18 +0700

    update

--- client/index.php
+++ client/index.php
@@ -1,3 +1,5 @@
+<?php include "navbar.php" ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,34 +24,6 @@
 
 <body style="font-family: 'Plus Jakarta Sans', sans-serif;">
     <header>
-        <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #fff;">
-            <div class="container d-flex justify-content-between">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <a class="navbar-brand" href="#"><img src="./bg/Logo.svg" alt="logo-way-kalam" height="60"></a>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto gap-3 d-flex align-items-center">
-                        <li class="nav-item">
-                            <a class="nav-link active text-white" aria-current="page" href="#">Beranda</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Tentang Kami</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Wisata</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white">UMKM</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white">Kontak Kami</a>
-                        </li>
-                        <button class="border-0 p-3 rounded-3" style="background: rgb(19,39,88); background: linear-gradient(313deg, rgba(19,39,88,1) 0%, rgba(0,166,147,1) 100%); color:white;">Map Interaktif<i class="fa-regular fa-map ps-3" style="color: #ffffff;"></i></button>
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
         <div id="latar" class="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
             <div id="latar" class="carousel slide" data-bs-ride="carousel">
